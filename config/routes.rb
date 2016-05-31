6983b64905bb5f2a57b081c089e0b57a7654154b
Matthijs Maas <Matthijs.M.Maas@gmail.com>
2016-05-31 14:41:36 +0200

    added the tasks model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,32 @@
 Rails.application.routes.draw do
+  get 'tasks/index'
+
+  get 'tasks/show'
+
+  get 'tasks/new'
+
+  get 'tasks/create'
+
+  get 'tasks/edit'
+
+  get 'tasks/update'
+
+  get 'tasks/destroy'
+
+  root 'tasks#index'
+
+  resources :tasks
+
+    # GET '/tasks': get all your tasks.
+    # GET '/tasks/:id': get a precise task, e.g GET '/tasks/33' get task with id=3
+    # GET '/tasks/new': get the form to create a new task
+    # POST '/tasks': post a new task
+    # GET '/tasks/:id/edit': get the form to edit an existing task
+    # PATCH '/tasks/:id': update an existing task
+    # DELETE '/tasks/:id': delete an existing task
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
